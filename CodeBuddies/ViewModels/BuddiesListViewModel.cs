12ca3af11e82a311c84d92da61bd525dffefb3bb
PotatoOverlord22 <tiron.raul22@gmail.com>
2024-04-18 14:58:48 +0300

    add buddies viewmodel

---
+++ CodeBuddies/ViewModels/BuddiesListViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CodeBuddies.Models;
+using CodeBuddies.MVVM;
+
+namespace CodeBuddies.ViewModels
+{
+    internal class BuddiesListViewModel : ViewModelBase
+    {
+        private List<Buddy> buddies;
+
+        public List<Buddy> Buddies
+        {
+            get { return buddies; }
+            set { buddies = value; OnPropertyChanged(); }
+        }
+
+
+        public BuddiesListViewModel()
+        {
+
+        }
+
+        public static void PopulateWithHardCodedBuddies()
+        {
+
+        }
+    }
+}
