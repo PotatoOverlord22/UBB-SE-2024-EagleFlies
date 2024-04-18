002c2ae6b82fdf803211ae9b6c31c43c0ffec00d
PotatoOverlord22 <tiron.raul22@gmail.com>
2024-04-18 16:23:40 +0300

    add BuddyRepository

---
+++ CodeBuddies/Repositories/BuddyRepository.cs
@@ -0,0 +1,45 @@
+﻿using CodeBuddies.Models;
+
+namespace CodeBuddies.Repositories
+{
+    internal class BuddyRepository
+    {
+        private string filePath;
+
+        public string FilePath
+        {
+            get { return filePath; }
+            set { filePath = value; }
+        }
+
+        private List<Buddy> buddies;
+
+        public List<Buddy> Buddies
+        {
+            get { return buddies; }
+            set { buddies = value; }
+        }
+
+        public BuddyRepository(string filePath)
+        {
+            FilePath = filePath;
+            Buddies = new List<Buddy>();
+            PopulateBuddiesFromFile();
+        }
+
+        public void PopulateBuddiesFromFile()
+        {
+            // Read from an xml file given by FilePath all the buddies and populate the list
+        }
+
+        public void SaveToFile()
+        {
+            // Save the buddies to the xml file given by FilePath
+        }
+
+        public List<Buddy> GetAll()
+        {
+            return Buddies;
+        }
+    }
+}
