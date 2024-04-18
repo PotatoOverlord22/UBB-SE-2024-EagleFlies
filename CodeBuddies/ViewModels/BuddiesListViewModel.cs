5d3a94fc687e46336be171911f8e9322653cdf8e
PotatoOverlord22 <tiron.raul22@gmail.com>
2024-04-18 15:50:47 +0300

    add BuddiesListViewModel, change buddies list view to listview

--- CodeBuddies/ViewModels/BuddiesListViewModel.cs
+++ CodeBuddies/ViewModels/BuddiesListViewModel.cs
@@ -1,13 +1,14 @@
 ﻿using CodeBuddies.Models;
 using CodeBuddies.MVVM;
+using System.Collections.ObjectModel;
 
 namespace CodeBuddies.ViewModels
 {
     internal class BuddiesListViewModel : ViewModelBase
     {
-        private List<Buddy> buddies;
+        private ObservableCollection<Buddy> buddies = new ObservableCollection<Buddy>();
 
-        public List<Buddy> Buddies
+        public ObservableCollection<Buddy> Buddies
         {
             get { return buddies; }
             set { buddies = value; OnPropertyChanged(); }
@@ -16,12 +17,14 @@ namespace CodeBuddies.ViewModels
 
         public BuddiesListViewModel()
         {
-
+            PopulateWithHardCodedBuddies();
         }
 
-        public static void PopulateWithHardCodedBuddies()
+        public void PopulateWithHardCodedBuddies()
         {
-
+            buddies.Add(new Buddy(1, "yo1", "pack://application:,,,/CodeBuddies;component/resources/pictures/dog_picture.png", "active", new List<Notification>()));
+            buddies.Add(new Buddy(2, "yo2", "pack://application:,,,/CodeBuddies;component/resources/pictures/dog_picture.png", "inactive", new List<Notification>()));
+            buddies.Add(new Buddy(3, "yo3", "pack://application:,,,/CodeBuddies;component/resources/pictures/dog_picture.png", "inactive", new List<Notification>()));
         }
     }
 }
