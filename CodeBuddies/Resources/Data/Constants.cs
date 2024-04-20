ac3f2f5ddf2bca05a9839b7deb198259ad8d871c
PotatoOverlord22 <tiron.raul22@gmail.com>
2024-04-20 19:51:08 +0300

    add database connection string

--- CodeBuddies/Resources/Data/Constants.cs
+++ CodeBuddies/Resources/Data/Constants.cs
@@ -9,7 +9,7 @@ namespace CodeBuddies.Resources.Data
     public static class Constants
     {
         public const int MAX_NUMBER_OF_BUDDIES_PER_SESSION = 20;
-        public const string CONNECTION_STRING = "data source=Cosmin\\SQLEXPRESS;initial catalog=CodeBuddiesDB;trusted_connection=true";
+        public const string CONNECTION_STRING = "data source=DESKTOP-CBK53R0\\SQLEXPRESS;initial catalog=CodeBuddiesDB;trusted_connection=true";
         public const long CLIENT_BUDDY_ID = 1;
     }
 }
